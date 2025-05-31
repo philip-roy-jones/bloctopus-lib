dc5944c49341cc1174d2d605b443ed5bab773886
Philip Roy Jones <145068574+philip-roy-jones@users.noreply.github.com>
2025-05-31 09:23:20 -0400

    refactor: update createRabbitChannel to accept connection parameters

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import amqp from 'amqplib';
 
-export async function createRabbitChannel() {
-  const connection = await amqp.connect('amqp://rabbitmq'); // hostname from docker-compose
+export async function createRabbitChannel(host: string, port: number, user: string, password: string) {
+  const connection = await amqp.connect({
+    hostname: host,
+    port: port,
+    username: user,
+    password: password,
+  });
   const channel = await connection.createChannel();
   return { connection, channel };
 }
